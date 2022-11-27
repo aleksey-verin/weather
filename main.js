27b7e4901d8e7c538b4c4095931ea9d7e6414601
Aleksey Verin <verevaa@yandex.ru>
2022-11-27 17:45:45 +0300

    мелкие правки

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 function openCity(event, nameOfTab) {
-    let tabcontent
-    let tablinks
+    
 
-    tabcontent = document.getElementsByClassName("tabcontent");
+    let tabcontent = document.getElementsByClassName("tabcontent");
     for (let i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
+    let tablinks = document.getElementsByClassName("tablinks");
     for (let i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
