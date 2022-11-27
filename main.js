ecf7998c94e93a48338b5b616dc4ae3489cae2db
Aleksey Verin <verevaa@yandex.ru>
2022-11-27 14:08:57 +0300

    Добавлен js для табов

---
+++ main.js
@@ -0,0 +1,15 @@
+function openCity(event, nameOfTab) {
+    let tabcontent
+    let tablinks
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(nameOfTab).style.display = "block";
+    event.currentTarget.className += " active";
+}
